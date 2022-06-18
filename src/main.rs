30b129c1173053a9246486b2064694c66eb32728
Siliwolf <reynardbrendan@gmail.com>
2022-06-18 16:19:09 -0300

    Added help command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use qc_bot::*;
 use serenity::async_trait;
 use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{CommandResult, StandardFramework, Args};
+use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+use serenity::utils::colours;
 
 use crate::money::BAL_COMMAND;
 use crate::money::PAY_COMMAND;
@@ -12,7 +13,7 @@ use crate::money::TRIVIA_COMMAND;
 use crate::money::REDEEM_COMMAND;
 
 #[group]
-#[commands(start_game, tax, bal, pay, trivia, redeem)]
+#[commands(start_game, tax, bal, pay, trivia, redeem, help)]
 struct General;
 
 struct Handler;
@@ -51,8 +52,31 @@ async fn main() {
 
 #[command]
 async fn start_game(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, format!("Starting game with mentionned players"))
+    msg.reply(ctx, format!("Starting game with mentioned players"))
         .await?;
+
+    Ok(())
+}
+
+#[command]
+async fn help(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel(ctx).await.unwrap().guild().unwrap().send_message(ctx, |m| {
+        m.content("")
+        .embed(|e| {
+            e.title("Help Menu (Note: Prefix in this is --, but it may be different)")
+            .field("**General Commands**", "help: *Shows this embed. Usage: --help*\n
+            ", false)
+
+            .field("**Economy Commands**", "tax: *Add or remove money from a user. Admin command. Usage: --tax <amount> <@User>*\n
+            bal: *Check how much money specified user has. Leave user field blank for yourself. Usage: --bal <@User>*\n
+            pay: *Pay money to another user. Usage: --pay <amount> <@User>*\n
+            trivia: *Get a trivia question. Costs $20. Usage: --trivia*\n
+            redeem: *Redeem your daily reward. Usage: --redeem*\n
+            ", false)
+            .colour(colours::roles::BLUE)
+        })
+    })
+    .await?;
 
     Ok(())
 }
