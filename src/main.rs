fe65fca51c49456e5ef98422997da8755782a910
Siliwolf <reynardbrendan@gmail.com>
2022-06-25 18:21:12 -0300

    Updated help menu

--- src/main.rs
+++ src/main.rs
@@ -70,21 +70,21 @@ async fn help(ctx: &Context, msg: &Message) -> CommandResult {
         m.content("")
         .embed(|e| {
             e.title("Help Menu (Note: Prefix in this is --, but it may be different)")
-            .field("**General Commands**", "help: *Shows this embed. Usage: --help*\n
+            .field("**General Commands**", "**help:** *Shows this embed. Usage: --help*\n
             ", false)
 
-            .field("**Base Economy Commands**", "tax: *Add or remove money from a user. Admin command. Usage: --tax <amount> <@User>*\n
-            bal: *Check how much money specified user has. Leave user field blank for yourself. Usage: --bal <@User>*\n
-            pay: *Pay money to another user. Usage: --pay <amount> <@User>*\n
-            trivia: *Get a trivia question. Costs $20. Usage: --trivia*\n
-            redeem: *Redeem your daily reward. Usage: --redeem*\n
-            leaderboard: *Show the top five richest users. Usage: --leaderboard*\n
+            .field("**Base Economy Commands**", "**tax:** *Add or remove money from a user. Admin command. Usage: --tax <amount> <@User>*\n
+            **bal:** *Check how much money specified user has. Leave user field blank for yourself. Usage: --bal <@User>*\n
+            **pay:** *Pay money to another user. Usage: --pay <amount> <@User>*\n
+            **trivia:** *Get a trivia question. Costs $20. Usage: --trivia*\n
+            **redeem:** *Redeem your daily reward. Usage: --redeem*\n
+            **leaderboard:** *Show the top five richest users. Usage: --leaderboard*\n
             ", false)
 
-            .field("Economy Shop Commands", "add_item: *Add an item to your shop. Only supports Discord built-in emojis. Usage: --add_item <name> <price> <emoji>*\n
-            remove_item: *Removes an item from your shop. Usage: --remove_item <name>*\n
-            items: *Check what items a shop has. Leave user field blank for yourself. Usage: --items <@User>*
-            buy: *Buy an item from another user. Usage: --buy <@User> <item_name>", false)
+            .field("**Economy Shop Commands**", "**add_item:** *Add an item to your shop. Only supports Discord built-in emojis. Usage: --add_item <name> <price> <emoji>*\n
+            **remove_item:** *Removes an item from your shop. Usage: --remove_item <name>*\n
+            **items:** *Check what items a shop has. Leave user field blank for yourself. Usage: --items <@User>*
+            **buy:** *Buy an item from another user. Usage: --buy <@User> <item_name>*", false)
             .colour(colours::roles::BLUE)
         })
     })
